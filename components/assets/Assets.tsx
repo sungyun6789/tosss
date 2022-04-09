ae358fb2c3af434ff71c4f59197797d06b75a945
sungyun <sungyun5423@gmail.com>
2022-04-09 23:47:25 +0900

    update: assets mock data

--- components/assets/Assets.tsx
+++ components/assets/Assets.tsx
@@ -6,11 +6,11 @@ const Assets = () => {
     <AssetsSection>
       <AssetsTitle>자산</AssetsTitle>
       <AssetsUL>
-        {mock.map(({ name, balance }) => (
-          <article key={name}>
+        {mock.map(({ wallet_name, balance }) => (
+          <article key={wallet_name}>
             <div>
               <p className="logo">Bank Image</p>
-              <li className="name">{name}</li>
+              <li className="name">{wallet_name}</li>
             </div>
             <li className="balance">{balance}</li>
           </article>
