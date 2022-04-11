e725bc98082794ffe7cab5b46bf2ba885d039a94
sungyun <sungyun5423@gmail.com>
2022-04-11 22:31:38 +0900

    feat: 계좌 상세 페이지 기본 레이아웃 구현

--- pages/assets/[...id].tsx
+++ pages/assets/[...id].tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
 
 import mock from '../../mock.json';
@@ -9,10 +10,41 @@ const AssetsPage = () => {
   const data = id ? mock.filter((data) => data.wallet_name === id[1])[0] : undefined;
 
   return (
-    <section>
-      <h1>{data?.wallet_name}</h1>
-    </section>
+    <>
+      <AssetsHeader>{`${data?.bank_name} ${data?.wallet_name}`}</AssetsHeader>
+      <AssetsInfoSection>
+        <span>{data?.address}</span>
+        <p className="balance">{data?.balance}</p>
+      </AssetsInfoSection>
+    </>
   );
 };
 
 export default AssetsPage;
+
+const AssetsHeader = styled.h1`
+  display: flex;
+  justify-content: center;
+  font-size: 20px;
+  padding-bottom: 50px;
+`;
+
+const AssetsInfoSection = styled.section`
+  width: 1000px;
+  margin: auto;
+
+  span {
+    display: flex;
+    justify-content: flex-end;
+
+    color: gray;
+    text-decoration: underline;
+  }
+  p {
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 30px;
+
+    font-size: 30px;
+  }
+`;
