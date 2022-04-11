fbc527ff53b79a33b14ca70efb5dba375f48da48
sungyun <sungyun5423@gmail.com>
2022-04-12 00:03:14 +0900

    feat: set recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import Layout from '@components/layout/Layout';
 
 import { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 
 import GlobalStyle from '../styles/global.style';
 
 const app = ({ Component, pageProps }: AppProps) => (
-  <Layout>
-    <GlobalStyle />
-    <Component {...pageProps} />
-  </Layout>
+  <RecoilRoot>
+    <Layout>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </Layout>
+  </RecoilRoot>
 );
 
 export default app;
