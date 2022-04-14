448a62f42c4dd5295ee7ff3d330e02d95985bef1
sungyun <sungyun5423@gmail.com>
2022-04-14 23:15:42 +0900

    feat: set page title & favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import Layout from '@components/layout/Layout';
 
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { RecoilRoot } from 'recoil';
 
+import favicon from '../public/favicon.ico';
 import GlobalStyle from '../styles/global.style';
 
 const app = ({ Component, pageProps }: AppProps) => (
   <RecoilRoot>
     <Layout>
       <GlobalStyle />
+      <Head>
+        <title>Tosss</title>
+        <link rel="icon" href={favicon.src} />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   </RecoilRoot>
