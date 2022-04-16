2b7da28946a2081176f5e68be6917b217c1e7c92
sungyun <sungyun5423@gmail.com>
2022-04-16 19:11:52 +0900

    feat: add mock data type

--- atoms/index.ts
+++ atoms/index.ts
@@ -2,6 +2,14 @@ import { atom } from 'recoil';
 
 import mock from '../mock.json';
 
+export interface AssetsModel {
+  wallet_name: string;
+  bank_name: string;
+  balance: number;
+  transfer: boolean;
+  address: string;
+}
+
 export const AssetsInfoState = atom({
   key: 'user',
   default: mock.sort((lhs, rhs) => lhs.wallet_name.localeCompare(rhs.wallet_name)),
