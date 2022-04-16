422bc7f7ed8eb1aa548a50023e943e2618bc7fdd
sungyun <sungyun5423@gmail.com>
2022-04-16 14:00:00 +0900

    feat: data sorting

--- atoms/index.ts
+++ atoms/index.ts
@@ -4,5 +4,5 @@ import mock from '../mock.json';
 
 export const AssetsInfoState = atom({
   key: 'user',
-  default: mock,
+  default: mock.sort((lhs, rhs) => lhs.wallet_name.localeCompare(rhs.wallet_name)),
 });
