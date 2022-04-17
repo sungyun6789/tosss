8c87fc7fd352a7294fcb99d3d4a71e781c315eed
sungyun <sungyun5423@gmail.com>
2022-04-17 23:24:10 +0900

    feat: 계좌 선택 로직 구현

--- components/select/BankSelector.tsx
+++ components/select/BankSelector.tsx
@@ -1,7 +1,25 @@
+import { AssetsModel } from 'atoms';
+
 import { BankSelectorWrapper } from './BankSelector.style';
 
-const BankSelector = () => {
-  return <BankSelectorWrapper>BankSelector</BankSelectorWrapper>;
+interface Props {
+  transferAssets: AssetsModel[];
+  setSelect: (select: string) => void;
+}
+
+const BankSelector = ({ transferAssets, setSelect }: Props) => {
+  return (
+    <BankSelectorWrapper>
+      <select onChange={(e) => setSelect(e.target.value)}>
+        <option value="">계좌를 선택하세요.</option>
+        {transferAssets.map(({ wallet_name }) => (
+          <option key={wallet_name} value={wallet_name}>
+            {wallet_name}
+          </option>
+        ))}
+      </select>
+    </BankSelectorWrapper>
+  );
 };
 
 export default BankSelector;
--- pages/transfer/index.tsx
+++ pages/transfer/index.tsx
@@ -1,18 +1,28 @@
+import { useState } from 'react';
+
 import DWButton from '@components/button/DWButton';
 import BankSelector from '@components/select/BankSelector';
+import { transferAssetsSelector } from 'atoms';
+
+import { useRecoilValue } from 'recoil';
 
 import { TransferPageWrapper } from './TransferPage.style';
 
 const TransferPage = () => {
+  const [depositSelect, setDepositSelect] = useState<string>();
+  const [withdrawalSelect, setWithdrawalSelect] = useState<string>();
+  const transferAssets = useRecoilValue(transferAssetsSelector);
+
   const deposit = () => undefined;
   const withdrawal = () => undefined;
 
   return (
     <TransferPageWrapper>
-      <BankSelector />
       <div>
         <DWButton deposit={deposit} withdrawal={withdrawal} />
       </div>
+      <BankSelector transferAssets={transferAssets} setSelect={setDepositSelect} />
+      <BankSelector transferAssets={transferAssets} setSelect={setWithdrawalSelect} />
     </TransferPageWrapper>
   );
 };
