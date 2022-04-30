f4006c876ef3502b621cfeae485d5c347b18ed06
sungyun <sungyun5423@gmail.com>
2022-04-30 21:11:24 +0900

    refactor: 메뉴 타입 고도화

--- pages/all/all.type.ts
+++ pages/all/all.type.ts
@@ -1,5 +1,20 @@
 export interface MenuModel {
-  category: string;
+  category:
+    | '최근'
+    | '신규'
+    | '대출'
+    | '보험'
+    | '송금'
+    | '결제'
+    | '계좌'
+    | '신용'
+    | '카드'
+    | '자동차'
+    | '부동산'
+    | '토스 주민센터'
+    | '혜택'
+    | '생활'
+    | '사장님';
   menu: {
     [key: string]: {
       url: string;
