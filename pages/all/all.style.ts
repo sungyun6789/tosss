556442b7d8e0428de8612670e7878a39924ca847
sungyun <sungyun5423@gmail.com>
2022-04-30 22:02:37 +0900

    style: 전체 페이지 초기 레이아웃 구현

---
+++ pages/all/all.style.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const AllPageWrapper = styled.section`
+  width: 1000px;
+  margin: auto;
+`;
+
+export const MenuWrapper = styled.div`
+  min-height: 100px;
+  border-bottom: 1px solid #f0f0f0;
+`;
+
+export const MenuTitle = styled.h3`
+  padding-top: 30px;
+  font-size: 20px;
+`;
--- pages/all/index.tsx
+++ pages/all/index.tsx
@@ -1,5 +1,16 @@
+import { MENU } from './all.constant';
+import { AllPageWrapper, MenuTitle, MenuWrapper } from './all.style';
+
 const AllPage = () => {
-  return <div>전체</div>;
+  return (
+    <AllPageWrapper>
+      {MENU.map((value) => (
+        <MenuWrapper key={value.category}>
+          <MenuTitle>{value.category}</MenuTitle>
+        </MenuWrapper>
+      ))}
+    </AllPageWrapper>
+  );
 };
 
 export default AllPage;
