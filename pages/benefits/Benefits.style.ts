d61c21111fe523a8a983a67109dbeb94bb0227ec
sungyun <sungyun5423@gmail.com>
2022-05-01 02:08:02 +0900

    style: add benefit page style

---
+++ pages/benefits/Benefits.style.ts
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+export const BenefitWrapper = styled.section`
+  width: 1000px;
+  margin: auto;
+
+  h1 {
+    text-align: center;
+    font-size: 30px;
+  }
+
+  h3 {
+    font-size: 20px;
+    text-align: center;
+    margin-top: 50px;
+  }
+
+  article {
+    padding: 20px;
+    border-bottom: 1px solid #f0f0f0;
+    cursor: pointer;
+  }
+`;
+
+export const FirstText = styled.div`
+  font-size: 14px;
+  color: #dee0e6;
+  margin-bottom: 10px;
+`;
+
+export const SecondText = styled.div`
+  font-size: 16px;
+  color: #2196f3;
+`;
--- pages/benefits/index.tsx
+++ pages/benefits/index.tsx
@@ -1,32 +1,55 @@
+import { BenefitWrapper, FirstText, SecondText } from './Benefits.style';
+
 const BenefitsPage = () => {
   return (
-    <>
+    <BenefitWrapper>
       <h1>혜택</h1>
-      <div>내 포인트</div>
-      <div>10원</div>
+      <article>
+        <FirstText>내 포인트</FirstText>
+        <SecondText>10원</SecondText>
+      </article>
 
       <h3>포인트 모으기</h3>
-      <div>좋아하는 브랜드에서</div>
-      <div>브랜드 캐시백 받기</div>
+      <article>
+        <FirstText>만보기</FirstText>
+        <SecondText>140원 받기</SecondText>
+      </article>
 
-      <div>이번 주 미션하면</div>
-      <div>얼마 받을지 보기</div>
+      <article>
+        <FirstText>좋아하는 브랜드에서</FirstText>
+        <SecondText>브랜드 캐시백 받기</SecondText>
+      </article>
 
-      <div>머니 알림 받고</div>
-      <div>20원 받기</div>
+      <article>
+        <FirstText>이번 주 미션하면</FirstText>
+        <SecondText>얼마 받을지 보기</SecondText>
+      </article>
 
-      <div>토스페이 결재할 때</div>
-      <div>할인 캐시백 받기</div>
+      <article>
+        <FirstText>머니 알림 받고</FirstText>
+        <SecondText>20원 받기</SecondText>
+      </article>
 
-      <div>택시 타고</div>
-      <div>6,000원 받기</div>
+      <article>
+        <FirstText>토스페이 결재할 때</FirstText>
+        <SecondText>할인 캐시백 받기</SecondText>
+      </article>
 
-      <div>계좌 만들고</div>
-      <div>최대 20,000원 받기</div>
+      <article>
+        <FirstText>택시 타고</FirstText>
+        <SecondText>6,000원 받기</SecondText>
+      </article>
 
-      <div>카드 쓰고</div>
-      <div>50,000 받기</div>
-    </>
+      <article>
+        <FirstText>계좌 만들고</FirstText>
+        <SecondText>최대 20,000원 받기</SecondText>
+      </article>
+
+      <article>
+        <FirstText>카드 쓰고</FirstText>
+        <SecondText>50,000 받기</SecondText>
+      </article>
+    </BenefitWrapper>
   );
 };
 
