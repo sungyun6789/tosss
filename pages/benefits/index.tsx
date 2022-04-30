ab73ccbafd1ee8b71d93f0b43656a8f383d92d93
sungyun <sungyun5423@gmail.com>
2022-04-30 23:29:35 +0900

    feat: 혜택 페이지 마크업

--- pages/benefits/index.tsx
+++ pages/benefits/index.tsx
@@ -1,5 +1,33 @@
 const BenefitsPage = () => {
-  return <div>혜택</div>;
+  return (
+    <>
+      <h1>혜택</h1>
+      <div>내 포인트</div>
+      <div>10원</div>
+
+      <h3>포인트 모으기</h3>
+      <div>좋아하는 브랜드에서</div>
+      <div>브랜드 캐시백 받기</div>
+
+      <div>이번 주 미션하면</div>
+      <div>얼마 받을지 보기</div>
+
+      <div>머니 알림 받고</div>
+      <div>20원 받기</div>
+
+      <div>토스페이 결재할 때</div>
+      <div>할인 캐시백 받기</div>
+
+      <div>택시 타고</div>
+      <div>6,000원 받기</div>
+
+      <div>계좌 만들고</div>
+      <div>최대 20,000원 받기</div>
+
+      <div>카드 쓰고</div>
+      <div>50,000 받기</div>
+    </>
+  );
 };
 
 export default BenefitsPage;
