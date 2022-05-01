88d9b98eb5b2cea92a4545180422db9cdfe284ad
sungyun <sungyun5423@gmail.com>
2022-05-01 20:51:56 +0900

    feat: 토스 익명 송금 링크 연결

--- components/button/SponsoredButton/SponsoredButton.tsx
+++ components/button/SponsoredButton/SponsoredButton.tsx
@@ -3,7 +3,9 @@ import { SponsoredButtonWrapper } from './SponsoredButton.style';
 const SponsoredButton = () => {
   return (
     <SponsoredButtonWrapper>
-      <button>후원</button>
+      <a target="_blank" href="https://toss.me/sungyun67" rel="noreferrer">
+        <button>후원하기</button>
+      </a>
     </SponsoredButtonWrapper>
   );
 };
