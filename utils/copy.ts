5342fc6d3a39715875839bc1f520a8de3617919f
sungyun <sungyun5423@gmail.com>
2022-05-01 23:59:45 +0900

    refactor: copy 함수에 alert 추가

--- utils/copy.ts
+++ utils/copy.ts
@@ -1,3 +1,7 @@
-const copy = (text: string) => navigator.clipboard.writeText(text);
+const copy = (text: string) =>
+  navigator.clipboard
+    .writeText(text)
+    .then(() => alert('계좌 번호가 복사되었습니다.'))
+    .catch(() => alert('다시 시도해주세요.'));
 
 export default copy;
