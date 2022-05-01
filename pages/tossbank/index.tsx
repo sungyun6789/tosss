5c1e733b1a39a3488ff4079c0e45b333609c9ced
sungyun <sungyun5423@gmail.com>
2022-05-01 22:48:17 +0900

    feat: tossbank page markup

--- pages/tossbank/index.tsx
+++ pages/tossbank/index.tsx
@@ -1,5 +1,43 @@
 const TossBankPage = () => {
-  return <div>토스뱅크</div>;
+  return (
+    <>
+      <h1>토스뱅크</h1>
+      <div>BankImage</div>
+      <p>토스뱅크 통장</p>
+      <p>3,000원</p>
+      <ul>
+        <li>
+          <p>토스뱅크 대출 한도</p>
+          <p>14,000,000 원</p>
+        </li>
+        <li>
+          <p>토스뱅크카드</p>
+          <p>0 원</p>
+        </li>
+        <li>
+          <p>돈 모으기</p>
+          <p>1,000 원</p>
+        </li>
+      </ul>
+      <h3>토스뱅크 둘러보기</h3>
+      <div>
+        <p>토스뱅크 신용대출</p>
+        <div>내 대출한도 보기</div>
+      </div>
+      <div>
+        <p>매일 300원 바로 받는 캐시백</p>
+        <div>토스뱅크카드 구경하기</div>
+      </div>
+      <div>
+        <p>하루만 넣어도 이자가 쌓이는 통장</p>
+        <div>통장 만들기</div>
+      </div>
+      <div>
+        <p>나한테 딱 맞는 돈 모으기</p>
+        <div>돈 모으기</div>
+      </div>
+    </>
+  );
 };
 
 export default TossBankPage;
