9a771c3db9f87a3849591cb91549c901e45ee789
sungyun <sungyun5423@gmail.com>
2022-05-01 23:51:11 +0900

    style: 전체적인 레이아웃 잡음

---
+++ pages/tossbank/TossBankPage.style.ts
@@ -0,0 +1,60 @@
+import styled from '@emotion/styled';
+
+export const TossBankWrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  width: 1000px;
+  margin: auto;
+
+  h1 {
+    font-size: 25px;
+    text-align: center;
+    padding: 20px;
+  }
+
+  h3 {
+    font-size: 20px;
+    text-align: center;
+    padding: 20px;
+  }
+
+  ul {
+    margin-top: 50px;
+  }
+
+  li {
+    height: 50px;
+  }
+`;
+
+export const TossBankImage = styled.article`
+  width: 100px;
+  height: 100px;
+  margin: auto;
+  border-radius: 50%;
+  border: 1px solid #f0f0f0;
+`;
+
+export const TossBankInfo = styled.article`
+  text-align: center;
+  padding: 10px;
+  p,
+  span {
+    padding: 10px;
+  }
+
+  span {
+    font-size: 25px;
+    font-weight: bold;
+  }
+`;
+
+export const TossBankBox = styled.article`
+  width: 400px;
+  height: 400px;
+  border: 1px solid #f0f0f0;
+  border-radius: 30px;
+  box-sizing: border-box;
+  margin-bottom: 10px;
+  padding: 20px;
+`;
--- pages/tossbank/index.tsx
+++ pages/tossbank/index.tsx
@@ -1,42 +1,49 @@
+import { TossBankBox, TossBankImage, TossBankInfo, TossBankWrapper } from './TossBankPage.style';
+
 const TossBankPage = () => {
   return (
-    <>
-      <h1>토스뱅크</h1>
-      <div>BankImage</div>
-      <p>토스뱅크 통장</p>
-      <p>3,000원</p>
-      <ul>
-        <li>
-          <p>토스뱅크 대출 한도</p>
-          <p>14,000,000 원</p>
-        </li>
-        <li>
-          <p>토스뱅크카드</p>
-          <p>0 원</p>
-        </li>
-        <li>
-          <p>돈 모으기</p>
-          <p>1,000 원</p>
-        </li>
-      </ul>
-      <h3>토스뱅크 둘러보기</h3>
-      <div>
-        <p>토스뱅크 신용대출</p>
-        <div>내 대출한도 보기</div>
-      </div>
-      <div>
-        <p>매일 300원 바로 받는 캐시백</p>
-        <div>토스뱅크카드 구경하기</div>
-      </div>
-      <div>
-        <p>하루만 넣어도 이자가 쌓이는 통장</p>
-        <div>통장 만들기</div>
-      </div>
-      <div>
-        <p>나한테 딱 맞는 돈 모으기</p>
-        <div>돈 모으기</div>
-      </div>
-    </>
+    <TossBankWrapper>
+      <article>
+        <h1>토스뱅크</h1>
+        <TossBankImage />
+        <TossBankInfo>
+          <p>토스뱅크 통장</p>
+          <span>3,000원</span>
+        </TossBankInfo>
+
+        <ul>
+          <li>
+            <p>토스뱅크 대출 한도</p>
+            <p>14,000,000 원</p>
+          </li>
+          <li>
+            <p>토스뱅크카드</p>
+            <p>0 원</p>
+          </li>
+          <li>
+            <p>돈 모으기</p>
+            <p>1,000 원</p>
+          </li>
+        </ul>
+        <h3>토스뱅크 둘러보기</h3>
+        <TossBankBox>
+          <p>토스뱅크 신용대출</p>
+          <div>내 대출한도 보기</div>
+        </TossBankBox>
+        <TossBankBox>
+          <p>매일 300원 바로 받는 캐시백</p>
+          <div>토스뱅크카드 구경하기</div>
+        </TossBankBox>
+        <TossBankBox>
+          <p>하루만 넣어도 이자가 쌓이는 통장</p>
+          <div>통장 만들기</div>
+        </TossBankBox>
+        <TossBankBox>
+          <p>나한테 딱 맞는 돈 모으기</p>
+          <div>돈 모으기</div>
+        </TossBankBox>
+      </article>
+    </TossBankWrapper>
   );
 };
 
