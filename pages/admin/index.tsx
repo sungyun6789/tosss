3266392fb046959d07c78a779104ed9f3fb945ef
sungyun <sungyun5423@gmail.com>
2022-05-03 00:08:11 +0900

    feat: 관리자 입금도 기록 남기도록 함

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import { assetsState } from 'atoms';
+import date from 'utils/date';
 
 import { useRecoilState } from 'recoil';
 
@@ -12,21 +13,40 @@ const AdminPage = () => {
 
   const onClick = () => {
     if (price) {
-      setAssets([
-        ...assets.map((value) =>
-          value.wallet_name === '토스뱅크' ? { ...value, balance: value.balance + price } : value,
-        ),
-      ]);
+      setAssets(
+        assets.map((value) => {
+          if (value.wallet_name === '토스뱅크') {
+            return {
+              ...value,
+              balance: value.balance + price,
+              details: [
+                ...value.details!,
+                {
+                  id: value.details!.length + 1,
+                  name: '관리자 입금',
+                  date,
+                  balance: price,
+                  type: 'deposit',
+                },
+              ],
+            };
+          } else {
+            return value;
+          }
+        }),
+      );
+      alert(`토스뱅크에 ${price}원 입금되었습니다.`);
+      setPrice(undefined);
     }
   };
 
   return (
     <AdminPageWrapper>
       <article>
-        <input placeholder="단위: 원" onChange={(e) => setPrice(+e.target.value)} />
+        <input placeholder="단위: 원" type="number" value={price ?? ''} onChange={(e) => setPrice(+e.target.value)} />
       </article>
       <article>
-        <button onClick={onClick}>충전</button>
+        <button onClick={onClick}>토스뱅크에 입금</button>
       </article>
     </AdminPageWrapper>
   );
