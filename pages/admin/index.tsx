309aea621e8fdf32e84b6849f10e6250b1418f00
sungyun <sungyun5423@gmail.com>
2022-05-02 23:59:03 +0900

    feat: 토스뱅크에 잔고 충전하는 기능 구현

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
+
+import { assetsState } from 'atoms';
+
+import { useRecoilState } from 'recoil';
+
 import { AdminPageWrapper } from './AdminPage.style';
 
 const AdminPage = () => {
+  const [price, setPrice] = useState<number>();
+  const [assets, setAssets] = useRecoilState(assetsState);
+
+  const onClick = () => {
+    if (price) {
+      setAssets([
+        ...assets.map((value) =>
+          value.wallet_name === '토스뱅크' ? { ...value, balance: value.balance + price } : value,
+        ),
+      ]);
+    }
+  };
+
   return (
     <AdminPageWrapper>
       <article>
-        <input placeholder="단위: 원" />
+        <input placeholder="단위: 원" onChange={(e) => setPrice(+e.target.value)} />
       </article>
       <article>
-        <button>충전</button>
+        <button onClick={onClick}>충전</button>
       </article>
     </AdminPageWrapper>
   );
