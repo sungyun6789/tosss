b0fa0257e8e1bcd77ba98e6a2a3f8601c97c6b06
sungyun <sungyun5423@gmail.com>
2022-05-04 22:25:48 +0900

    feat: 송금 커스텀 훅 구현

---
+++ hooks/useTransfer.ts
@@ -0,0 +1,55 @@
+import { assetsState } from 'atoms';
+import date from 'utils/date';
+
+import { useRecoilState } from 'recoil';
+
+interface Props {
+  price: number;
+  depositBank: string;
+  withdrawalBank?: string;
+  isAdmin?: boolean;
+}
+
+const useTransfer = ({ price, depositBank, withdrawalBank = '', isAdmin = false }: Props) => {
+  const [assets, setAssets] = useRecoilState(assetsState);
+
+  return setAssets(
+    assets.map((value) => {
+      if (value.wallet_name === depositBank) {
+        return {
+          ...value,
+          balance: value.balance + price,
+          details: [
+            {
+              id: value.details!.length + 1,
+              name: isAdmin ? withdrawalBank : '관리자 입금',
+              date,
+              balance: price,
+              type: 'withdrawal',
+            },
+            ...value.details!,
+          ],
+        };
+      } else if (value.wallet_name === withdrawalBank) {
+        return {
+          ...value,
+          balance: value.balance + price,
+          details: [
+            {
+              id: value.details!.length + 1,
+              name: depositBank,
+              date,
+              balance: price,
+              type: 'deposit',
+            },
+            ...value.details!,
+          ],
+        };
+      } else {
+        return value;
+      }
+    }),
+  );
+};
+
+export default useTransfer;
